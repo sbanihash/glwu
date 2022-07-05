699a0b33c36b0007516be25056a6e0785243ad87
awest-noaa <andre.vanderwesthuysen@noaa.gov>
2022-07-05 17:27:44 +0000

    Updated username to lower case

--- sorc/get_externals.sh
+++ sorc/get_externals.sh
@@ -3,11 +3,11 @@
 #                                                                             #
 # Copy external fix files that are too large to store in repository           #
 #                                                                             #
-# Last Changed : 09-27-2021                                   September 2021  #
+# Last Changed : 07-05-2022                                        July 2022  #
 # --------------------------------------------------------------------------- #
 
 echo 'Fetching externals...'
-cp -p /lfs/h2/emc/couple/noscrub/Andre.VanderWesthuysen/git/fv3gfs/fix/fix_glwu/mesh.glwu ../fix/
-cp -p /lfs/h2/emc/couple/noscrub/Andre.VanderWesthuysen/git/fv3gfs/fix/fix_glwu/grint_weights.grlc_2p5km ../fix/
-cp -p /lfs/h2/emc/couple/noscrub/Andre.VanderWesthuysen/git/fv3gfs/fix/fix_glwu/grint_weights.grlr ../fix/
-cp -p /lfs/h2/emc/couple/noscrub/Andre.VanderWesthuysen/git/fv3gfs/fix/fix_glwu/grint_weights.grlr_500m ../fix/
+cp -p /lfs/h2/emc/couple/noscrub/andre.vanderwesthuysen/git/fv3gfs/fix/fix_glwu/mesh.glwu ../fix/
+cp -p /lfs/h2/emc/couple/noscrub/andre.vanderwesthuysen/git/fv3gfs/fix/fix_glwu/grint_weights.grlc_2p5km ../fix/
+cp -p /lfs/h2/emc/couple/noscrub/andre.vanderwesthuysen/git/fv3gfs/fix/fix_glwu/grint_weights.grlr ../fix/
+cp -p /lfs/h2/emc/couple/noscrub/andre.vanderwesthuysen/git/fv3gfs/fix/fix_glwu/grint_weights.grlr_500m ../fix/
